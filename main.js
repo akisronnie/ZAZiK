e4c9ba45f164ee90261b53e2443aa85e6ba8dab7
akisronnie <akisronnie@gmail.com>
2018-05-14 23:51:02 +0300

    add icons

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function printTime() {
  if (h<10) h="0"+h;
  if (m<10) m="0"+m;
  if (s<10) s="0"+s;
- document.getElementById('clock').innerHTML=h+":"+m+":"+s;
+ document.getElementById('clock').innerHTML=" "+h+":"+m+":"+s;
 
 }
 setInterval(printTime, 1000);
